1e9595747574345b1ba4e56cea79bf29b57d7807
Paola Soto <paola.herso@gmail.com>
2016-08-13 14:06:31 -0500

    Add files via upload
    This assignment is to write a pair of functions that cache the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,10 @@
+#Matrix inversion is usually a costly computation and 
+#there may be some benefit to caching the inverse of
+#a matrix rather than compute it repeatedly.
+# The assignment is to write a pair of functions 
+#that cache the inverse of a matrix.
+
+
 #The first function, makeVector creates a special "vector", 
 #which is really a list containing a function to:
 #Set and get the value of the vector, and the second function
